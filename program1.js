494b75c87902ab176649b314b1e0693f88da922e
Praveen Chandra Thapliyal <pthapliyal781@gmail.com>
2017-02-24 00:38:59 +0530

    c1

---
+++ program1.js
@@ -0,0 +1,15 @@
+
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server is running at http://${hostname}:${port}/`);
+});
